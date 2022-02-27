2d9bb80a408388916a7da7a487f076d808154fde
Danila Shloma <ds.shloma@gmail.com>
2022-02-28 00:04:31 +0300

    fix books without img , add book on focus , add authors and categories

--- src/Books/Book/Book.js
+++ src/Books/Book/Book.js
@@ -8,9 +8,18 @@ class Book extends React.Component {
         const {bookInfo} = this.props
         console.log(bookInfo)
         return (
-            <div className="book">
-                <p>{bookInfo.volumeInfo.title}</p>
-                 <img src = {bookInfo.volumeInfo.imageLinks.thumbnail} />  
+            <div className="book">    
+                 { 'imageLinks' in bookInfo.volumeInfo && <img src = {bookInfo.volumeInfo.imageLinks.thumbnail} /> || <img src="https://v1.iconsearch.ru/uploads/icons/token/128x128/search.png" />}
+                 <p className="categories">{bookInfo.volumeInfo.categories}</p>
+                 <p className="title-book">{bookInfo.volumeInfo.title}</p> 
+                 {bookInfo.volumeInfo.authors && bookInfo.volumeInfo.authors.map((item , index) => {
+                     if(index === 0){
+                        return <span className="authors" key={index}>{item}</span>    
+                     }else{
+                        return <span className="authors" key={index}> , {item}</span>  
+                     }
+                     
+                 })} 
             </div>
         ) 
     }
