14a5fb84222f53226e220f5c7a85564080132d2a
Danila Shloma <ds.shloma@gmail.com>
2022-02-27 15:22:05 +0300

    small visual update

--- src/Books/Book/Book.js
+++ src/Books/Book/Book.js
@@ -6,9 +6,11 @@ class Book extends React.Component {
 
     render(){
         const {bookInfo} = this.props
+        console.log(bookInfo)
         return (
             <div className="book">
                 <p>{bookInfo.volumeInfo.title}</p>
+                 <img src = {bookInfo.volumeInfo.imageLinks.thumbnail} />  
             </div>
         ) 
     }
